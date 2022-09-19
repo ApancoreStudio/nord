126630cde27d50473076588ce39941757e957868
Van <vanicgame@yandex.ru>
2022-09-19 21:04:22 +0300

    Добавление стандарта мышления нейтральный, попытки заставить его работать

--- mods/ENTITIES/entities_mobs/mobs_api.lua
+++ mods/ENTITIES/entities_mobs/mobs_api.lua
@@ -9,6 +9,12 @@ entities_mobs.register_way_of_thinking = function(name, thinking)
 	entities_mobs.ways_of_thinking[name] = thinking
 end
 
+local function round(num, numDecimalPlaces)
+	return tonumber(string.format("%." .. (numDecimalPlaces or 0) .. "f", num))
+end
+
+-- Функция, которая определяет поведение моба,
+-- согласно способу мышления и текущему мышлению.
 local function mob_act(self)
 	if not self then
 		return
@@ -45,7 +51,7 @@ entities_mobs.register_mob = function(name, def)
 		_is_mob = true,
 
 		-- Необязательные в def
-		stepheight = def.stepheight or 1,
+		stepheight = def.stepheight or 1.5,
 
 		-- Передаются в def
 		max_hp = def.max_hp,
@@ -57,15 +63,63 @@ entities_mobs.register_mob = function(name, def)
 
 		_breath_max = def.breath_max,
 		_armor = def.armor,
-		_fraction = def.fraction,
-		_friendly_fraction = def.friendly_fraction,
-		_enemy_fraction = def.enemy_fraction,
-		_review = def.review,
-		_way_of_thinking = def.way_of_thinking,
+		_fraction = def.fraction, -- str
+		_friendly_fraction = def.friendly_fraction, -- str
+		_enemy_fraction = def.enemy_fraction, -- str
+		_review = def.review, -- num
+		_way_of_thinking = def.way_of_thinking, -- str
+		_animations = def.animations, -- table
 
 		-- Функции
 		on_step = function(self, dtime)
 			mob_act(self)
 		end,
+
+		on_activate = function(self, staticdata, dtime_s)
+			self.object:set_acceleration({x = 0, y = -9.81, z = 0})
+		end,
 	})
+end
+
+-- Определить вектор скорости для достижения координаты
+-- в горизонтальной плоскости по прямой.
+function entities_mobs.get_target_horizontally_vel(mob_pos, target_pos)
+	local z = target_pos.z - mob_pos.z
+	local x = target_pos.x - mob_pos.x
+	local s = math.sqrt(x*x+z*z)
+	local vel = {x = round(x/s, 3), z = round(z/s, 3), y = 0}
+	return vel
+end
+
+-- Установить скорость равную speed по направлению
+-- в горизонтальной плоскости к координатам.
+function entities_mobs.set_target_horizontally_vel(obj_mob, target_pos, speed)
+	local vector_vel = entities_mobs.get_target_horizontally_vel(obj_mob:get_pos(), target_pos)
+	local vel = {x = vector_vel.x*speed, z = vector_vel.z*speed, y = 0}
+	obj_mob:set_velocity(vel)
+end
+
+-- Установить анимацию anim_name.
+-- Множитель скорости speed_mult необязателен (равен 1)
+entities_mobs.set_animation = function(self, anim_name, speed_mult)
+	local animations = self._animations
+	if not animations then
+		minetest.log("warning", "Попытка вызвать анимацию у "..
+			self.get_entity_name()..
+			". Осутствует список анимаций!")
+		return false
+	end
+
+	local frame_range = animations[anim_name]
+	if not frame_range then
+		minetest.log("warning", "Попытка вызвать анимацию у "..
+			self.get_entity_name()..
+			". Осутствует анимация "..anim_name" !")
+		return false
+	end
+
+	local frame_speed = (speed_mult or 1)*animations["frame_speed"]
+
+	self.object:set_animation(frame_range, frame_speed)
+	return true
 end
--- mods/ENTITIES/entities_mobs/ways_of_thinking.lua
+++ mods/ENTITIES/entities_mobs/ways_of_thinking.lua
@@ -43,11 +43,71 @@ entities_mobs.register_way_of_thinking("standart", {
 	["aggressive"] = standart_aggressive,
 })
 
+local function set_random_target_pos(self)
+	local mob_pos = self.object:get_pos()
+	local mob_review = self._review
+
+	local pos1 = {
+		x = mob_pos.x+mob_review,
+		y = mob_pos.y+1,
+		z = mob_pos.z+mob_review,
+	}
+	local pos2 = {
+		x = mob_pos.x-mob_review,
+		y = mob_pos.y-1,
+		z = mob_pos.z-mob_review,
+	}
+
+	targets_pos = minetest.find_nodes_in_area(pos1, pos2, {"air"}, false)
+
+	if not targets_pos then
+		return false
+	end
+
+	self._target = targets_pos[math.random(#targets_pos)]
+	return true
+end
+
+local function round(num, numDecimalPlaces)
+	return tonumber(string.format("%." .. (numDecimalPlaces or 0) .. "f", num))
+end
+
 -- Поведение мобов neutral
 local function neutral_stand(self)
+	minetest.log("stand")
+	if math.random(100) > 95 then
+		if set_random_target_pos(self) then
+			self._thinking = "wandering"
+		end
+	end
 end
 
 local function neutral_wandering(self)
+	minetest.log("wandeting")
+	local path = minetest.find_path(
+		self.object:get_pos(),
+		self._target,
+		self._review*1.5,
+		self.stepheight+2,
+		2)
+
+	if not path or #path == 0 then
+		self._thinking = "stand"
+		self.object:set_velocity({x = 0, y = 0, z = 0})
+		return
+	elseif round(self._target.x, 0) == round(self.object:get_pos().x,0) and
+		round(self._target.z, 0) == round(self.object:get_pos().z,0) then
+		self._thinking = "stand"
+		self.object:set_velocity({x = 0, y = 0, z = 0})
+		return
+	end
+
+	entities_mobs.set_target_horizontally_vel(self.object, self._target, 2)
+
+	if self.object:get_velocity().x == 0 and self.object:get_velocity().z == 0 and self.object:get_velocity().y == 0 then
+		self._thinking = "stand"
+		return
+	end
 end
 
 entities_mobs.register_way_of_thinking("neutral", {
