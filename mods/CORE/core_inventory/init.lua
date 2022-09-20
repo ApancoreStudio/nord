ac8c5f9fc77615089727a04bc8fb4e2010388429
Van <82447582+VanicGame@users.noreply.github.com>
2022-09-20 19:11:28 +0300

    Начало инвентаря (#17)
    * Начало инвентаря
    
    * luacheck

--- mods/CORE/core_inventory/init.lua
+++ mods/CORE/core_inventory/init.lua
@@ -3,50 +3,81 @@
 core_inventory = {}
 
 core_inventory.tabs_button =
-		"image_button[0,-1;1,1;core_inventory_craft_tab.png;tab_main;M;true;false]"..
-		"image_button[1,-1;1,1;core_inventory_equip_tab.png;tab_equipment;E;true;false]"
+	"image_button[-1,0;1,2;core_inventory_craft_tab.png;tab_craft;;true;false]"..
+	"image_button[-1,2;1,2;core_inventory_equip_tab.png;tab_equipment;;true;false]"
 
-core_inventory.storage = function(player)
+-- Вкладка крафта
+core_inventory.craft_formspec =
+	"list[current_player;craft;2,0.5;3,3;]"..
+	"list[current_player;craftpreview;5,1.5;1,1;]"
+
+core_inventory.storage_craft_formspec = function(player)
 	local inv = player:get_inventory()
 	local right_hipbag_size = inv:get_stack("equip_hipbag_right", 1):get_definition().groups.hipbag
 	local left_hipbag_size = inv:get_stack("equip_hipbag_left", 1):get_definition().groups.hipbag
 
-	local formspec = "list[current_player;main;7.5,0;4,6;]"
+	local formspec = "list[current_player;main;1.5,4;4,6;]"
 	if not inv:is_empty("equip_hipbag_right") then
-		formspec = formspec.."list[current_player;hipbag_right;7.5,5;2,"..right_hipbag_size..";]"
+		formspec = formspec.."list[current_player;hipbag_right;6,4;2,"..right_hipbag_size..";]"
 	end
 
 	if not inv:is_empty("equip_hipbag_left") then
-		formspec = formspec.."list[current_player;hipbag_left;9.5,5;2,"..left_hipbag_size..";]"
+		formspec = formspec.."list[current_player;hipbag_left;8,4;2,"..left_hipbag_size..";]"
 	end
 
 	return formspec
 end
 
-core_inventory.main_inv =
-		"image[1,0.6;1,2;player.png]"..
-		"list[current_player;craft;2,3.5;3,3;]"..
-		"list[current_player;craftpreview;5,4.5;1,1;]"
+core_inventory.craft_tab = function(player)
+	return core_inventory.craft_formspec..core_inventory.storage_craft_formspec(player)
+end
 
-core_inventory.equipment_inv = "size[14,8]"..
-		"image[2,1.5;3,6;player.png]"..
-		"list[current_player;equip_hipbag_right;1,4.5;1,1;]"..
-		"list[current_player;equip_hipbag_left;4.5,4.5;1,1;]"
 
+-- Вкладка экпировки
+core_inventory.equip_formspec =
+	"image[7.5,1.5;3,6;player.png]"..
+	"listcolors[#75593caa;#a3907d;#281c0f]"..
+	"background[0,0;14,8;player_inventory_background.png;true]"..
+	"list[current_player;equip_hipbag_right;7.1,4.5;1,1;]"..
+	"list[current_player;equip_hipbag_left;9.5,4.5;1,1;]"
+
+core_inventory.storage_equip_formspec = function(player)
+	--local inv = player:get_inventory()
+	--local right_hipbag_size = inv:get_stack("equip_hipbag_right", 1):get_definition().groups.hipbag
+	--local left_hipbag_size = inv:get_stack("equip_hipbag_left", 1):get_definition().groups.hipbag
+
+	local formspec = "list[current_player;main;0.5,4;4,6;]"
+	--[[if not inv:is_empty("equip_hipbag_right") then
+		formspec = formspec.."list[current_player;hipbag_right;7.5,5;2,"..right_hipbag_size..";]"
+	end
+
+	if not inv:is_empty("equip_hipbag_left") then
+		formspec = formspec.."list[current_player;hipbag_left;9.5,5;2,"..left_hipbag_size..";]"
+	end --]]
+
+	return formspec
+end
+
+core_inventory.equip_tab = function(player)
+	return core_inventory.equip_formspec..core_inventory.storage_equip_formspec(player)
+end
+
+-- Инвентарь игрока
 core_inventory.player_inventory = function(player, fields)
 	local formspec = "size[14,8]"
 
 	if not fields then
-		formspec = formspec..core_inventory.main_inv
-	elseif fields.tab_main then
-		formspec = formspec..core_inventory.main_inv
+		formspec = formspec..core_inventory.craft_tab(player)
+	elseif fields.tab_craft then
+		formspec = formspec..core_inventory.craft_tab(player)
 	elseif fields.tab_equipment then
-		formspec = formspec..core_inventory.equipment_inv
+		formspec = formspec..core_inventory.equip_tab(player)
 	else
-		formspec = formspec..core_inventory.main_inv
+		formspec = formspec..core_inventory.craft_tab(player)
 	end
 
-	formspec = formspec..core_inventory.storage(player)..core_inventory.tabs_button
+	formspec = formspec..core_inventory.tabs_button
+	minetest.log(formspec)
 
 	return formspec
 end
@@ -99,6 +130,7 @@ core_inventory.set_inv_lists = function(inv)
 	inv:set_size("main", 16)
 	inv:set_size("equip_hipbag_right", 1)
 	inv:set_size("equip_hipbag_left", 1)
+	inv:set_size("craft", 16)
 end
 
 minetest.register_on_joinplayer(function(player)
