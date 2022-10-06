0956b878b17620f8257d71686d417e49765ba417
Van <vanicgame@yandex.ru>
2022-10-06 19:19:49 +0300

    Начало библиотеки крафтов

---
+++ mods/CORE/core_functions/core_crafts.lua
@@ -0,0 +1,14 @@
+-- Core crafts
+-- Создано для легкой поддержки пользовательских типов крафта
+
+core_functions.registered_crafts = {}
+
+--[[
+	def = {
+		output = "craftitem",
+		recipe = {craftitem1, craftitem2 ... craftitemN}
+	}
+--]]
+core_functions.register_craft = function(def)
+	local recipe = 
+end
