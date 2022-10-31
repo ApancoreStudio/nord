54fe0a99e6a687647609736f00d32eee1702b9e9
Van <82447582+VanicGame@users.noreply.github.com>
2022-10-31 14:10:36 +0300

    Генерация руд (#9)
    * Генерация угольной руды
    
    * Генерация оловянной руды
    
    * Генерация медной руды
    
    * Генерация золотой руды

--- mods/CORE/core_mapgen/init.lua
+++ mods/CORE/core_mapgen/init.lua
@@ -213,6 +213,142 @@ end
 
 register_interworld_stratum("items_stone:kharite", wherein_krarite, 87, -256)
 
+-- Ores
+-- Coal
+minetest.register_ore({
+	ore_type = "scatter",
+	ore = "items_stone:basalt_with_coal",
+	wherein = "items_stone:basalt",
+	clust_scarcity = 12*12*12,
+	clust_num_ores = 8,
+	clust_size = 3,
+	y_max = -64,
+	y_min = -84,
+})
+
+minetest.register_ore({
+	ore_type = "scatter",
+	ore = "items_stone:basalt_with_coal",
+	wherein = "items_stone:basalt",
+	clust_scarcity = 8*8*8,
+	clust_num_ores = 14,
+	clust_size = 3,
+	y_max = -84,
+	y_min = -98,
+})
+
+minetest.register_ore({
+	ore_type = "scatter",
+	ore = "items_stone:basalt_with_coal",
+	wherein = "items_stone:basalt",
+	clust_scarcity = 6*6*6,
+	clust_num_ores = 18,
+	clust_size = 4,
+	y_max = -98,
+	y_min = -128,
+})
+
+-- Tin
+minetest.register_ore({
+	ore_type = "scatter",
+	ore = "items_stone:basalt_with_tin",
+	wherein = "items_stone:basalt",
+	clust_scarcity = 16*16*16,
+	clust_num_ores = 4,
+	clust_size = 3,
+	y_max = -64,
+	y_min = -84,
+})
+
+minetest.register_ore({
+	ore_type = "scatter",
+	ore = "items_stone:basalt_with_tin",
+	wherein = "items_stone:basalt",
+	clust_scarcity = 12*12*12,
+	clust_num_ores = 6,
+	clust_size = 3,
+	y_max = -84,
+	y_min = -98,
+})
+
+minetest.register_ore({
+	ore_type = "scatter",
+	ore = "items_stone:basalt_with_tin",
+	wherein = "items_stone:basalt",
+	clust_scarcity = 8*8*8,
+	clust_num_ores = 8,
+	clust_size = 4,
+	y_max = -98,
+	y_min = -128,
+})
+
+-- Copper
+minetest.register_ore({
+	ore_type = "scatter",
+	ore = "items_stone:basalt_with_copper",
+	wherein = "items_stone:basalt",
+	clust_scarcity = 18*18*18,
+	clust_num_ores = 4,
+	clust_size = 3,
+	y_max = -64,
+	y_min = -84,
+})
+
+minetest.register_ore({
+	ore_type = "scatter",
+	ore = "items_stone:basalt_with_copper",
+	wherein = "items_stone:basalt",
+	clust_scarcity = 14*14*14,
+	clust_num_ores = 5,
+	clust_size = 3,
+	y_max = -84,
+	y_min = -98,
+})
+
+minetest.register_ore({
+	ore_type = "scatter",
+	ore = "items_stone:basalt_with_copper",
+	wherein = "items_stone:basalt",
+	clust_scarcity = 10*10*10,
+	clust_num_ores = 6,
+	clust_size = 4,
+	y_max = -98,
+	y_min = -128,
+})
+
+-- Gold
+minetest.register_ore({
+	ore_type = "scatter",
+	ore = "items_stone:basalt_with_gold",
+	wherein = "items_stone:basalt",
+	clust_scarcity = 20*20*20,
+	clust_num_ores = 4,
+	clust_size = 3,
+	y_max = -64,
+	y_min = -84,
+})
+
+minetest.register_ore({
+	ore_type = "scatter",
+	ore = "items_stone:basalt_with_gold",
+	wherein = "items_stone:basalt",
+	clust_scarcity = 18*18*18,
+	clust_num_ores = 5,
+	clust_size = 3,
+	y_max = -84,
+	y_min = -98,
+})
+
+minetest.register_ore({
+	ore_type = "scatter",
+	ore = "items_stone:basalt_with_gold",
+	wherein = "items_stone:basalt",
+	clust_scarcity = 12*12*12,
+	clust_num_ores = 6,
+	clust_size = 4,
+	y_max = -98,
+	y_min = -128,
+})
 
 minetest.register_ore({
 	ore_type = "scatter",
@@ -225,6 +361,7 @@ minetest.register_ore({
 	y_min = -256,
 })
 
+-- Decoration
 minetest.register_decoration({
 	name = "core_mapgen:oak_bush",
 	deco_type = "schematic",
